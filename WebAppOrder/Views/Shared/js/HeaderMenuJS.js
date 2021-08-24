1684800c0fee038aff0e03036add0d632ed2c2af
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2021-08-24 16:48:00 +0200

    added profile page frontend fro UserProfile branch

---
+++ WebAppOrder/Views/Shared/js/HeaderMenuJS.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener('load', () => {
+
+    const menuBtn = document.getElementById("menuBtn");
+    const menu = document.getElementById("menu");
+
+
+    menuBtn.addEventListener('click', function () {
+        (menu.style.display === 'block') ?
+            menu.style.display = 'none' :
+            menu.style.display = 'block';
+    });
+
+});
